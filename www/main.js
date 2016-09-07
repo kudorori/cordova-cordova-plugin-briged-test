d2b786829f4b4cc6d97ad8a6054278dc6a5e3021
sumingfeng <kudokawai@gmail.com>
2016-09-07 14:06:38 +0800

    add

--- www/main.js
+++ www/main.js
@@ -1,5 +1,11 @@
 exports.test=function(message){
 	console.log("call");
-	var b = cordova.exec(null, null, "main", "show", [message]);
+	var b = cordova.exec(
+	function(e){
+		console.log("call success",e);
+	}, 
+	function(e){
+		console.log("call error",e);
+	}, "main", "show", [message]);
 	console.log("called",b);
 }
